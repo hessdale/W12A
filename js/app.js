581c582ddcd21d9d100f09511b93c18975fc9632
dale <hessdale96@gmail.com>
2023-04-04 21:52:21 -0600

    added html for selected meal

---
+++ js/app.js
@@ -0,0 +1,49 @@
+let body_html = document.querySelector(`body`);
+
+function successFunction(response) {
+    for (i = 0; i < response.data.meals.length; i++) {
+        body_html.insertAdjacentHTML(`afterbegin`, `<h1>${response.data.meals[0].strMeal}</h1>
+        <img src="${response.data.meals[0].strMealThumb}" alt="random meal thumbnail" width="350px">
+        <h2>Cuisine: ${response.data.meals[0].strArea}</h2>
+        <h2>Category: ${response.data.meals[0].strCategory}</h2>
+        <h3>Ingredients</h3>
+        <p><b>${response.data.meals[0].strMeasure1}</b> ${response.data.meals[0].strIngredient1}</p>
+        <p><b>${response.data.meals[0].strMeasure2}</b> ${response.data.meals[0].strIngredient2}</p>
+        <p><b>${response.data.meals[0].strMeasure3}</b> ${response.data.meals[0].strIngredient3}</p>
+        <p><b>${response.data.meals[0].strMeasure4}</b> ${response.data.meals[0].strIngredient4}</p>
+        <p><b>${response.data.meals[0].strMeasure5}</b> ${response.data.meals[0].strIngredient5}</p>
+        <p><b>${response.data.meals[0].strMeasure6}</b> ${response.data.meals[0].strIngredient6}</p>
+        <p><b>${response.data.meals[0].strMeasure7}</b> ${response.data.meals[0].strIngredient7}</p>
+        <p><b>${response.data.meals[0].strMeasure8}</b> ${response.data.meals[0].strIngredient8}</p>
+        <p><b>${response.data.meals[0].strMeasure9}</b> ${response.data.meals[0].strIngredient9}</p>
+        <p><b>${response.data.meals[0].strMeasure10}</b> ${response.data.meals[0].strIngredient10}</p>
+        <p><b>${response.data.meals[0].strMeasure11}</b> ${response.data.meals[0].strIngredient11}</p>
+        <p><b>${response.data.meals[0].strMeasure12}</b> ${response.data.meals[0].strIngredient12}</p>
+        <p><b>${response.data.meals[0].strMeasure13}</b> ${response.data.meals[0].strIngredient13}</p>
+        <p><b>${response.data.meals[0].strMeasure14}</b> ${response.data.meals[0].strIngredient14}</p>
+        <p><b>${response.data.meals[0].strMeasure15}</b> ${response.data.meals[0].strIngredient15}</p>
+        <p><b>${response.data.meals[0].strMeasure16}</b> ${response.data.meals[0].strIngredient16}</p>
+        <p><b>${response.data.meals[0].strMeasure17}</b> ${response.data.meals[0].strIngredient17}</p>
+        <p><b>${response.data.meals[0].strMeasure18}</b> ${response.data.meals[0].strIngredient18}</p>
+        <p><b>${response.data.meals[0].strMeasure19}</b> ${response.data.meals[0].strIngredient19}</p>
+        <p><b>${response.data.meals[0].strMeasure20}</b> ${response.data.meals[0].strIngredient20}</p>
+        <h1>Instructions</h1>
+        <p>${response.data.meals[0].strInstructions}</p>
+        `)
+    };
+};
+function failureFunction(error) {
+    body_html.insertAdjacentHTML(`afterbegin`, `<h1>ERROR</h1>`)
+};
+
+function fetchfood(dettails) {
+    console.log(`fetching meal data`);
+    axios.request({
+        url: `https://www.themealdb.com/api/json/v1/1/random.php`
+    }).then(successFunction).catch(failureFunction);
+};
+
+let button = document.querySelector(`button`);
+
+button.addEventListener(`click`, fetchfood);
+
